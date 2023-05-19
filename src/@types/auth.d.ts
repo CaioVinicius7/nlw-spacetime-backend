bd228362838acd4172a719ac171c43ff38bc59b5
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-19 12:31:21 -0300

    feat: :label: Tipagem do token jwt

---
+++ src/@types/auth.d.ts
@@ -0,0 +1,15 @@
+import "@fastify/jwt";
+
+declare module "@fastify/jwt" {
+  export interface FastifyJWT {
+    payload: {
+      name: string;
+      avatarUrl: string;
+    };
+    user: {
+      sub: string;
+      name: string;
+      avatarUrl: string;
+    };
+  }
+}
